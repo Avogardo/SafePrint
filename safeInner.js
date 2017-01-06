25581abd2e0ba56ca23a7207a410f70c2c1455ab
Avogardo <avogardo0@gmail.com>
2017-01-06 23:17:53 +0100

    safe-print
    Działa tylko [b] lub tylko [i]

--- safeInner.js
+++ safeInner.js
@@ -1,41 +1,17 @@
-const text = 'This is a [b]test[/b], all [b]html[/b] tags should <b>not</b> working';
+const text = 'This is a [i]test[/i], all [b]html[/b] tags<br>should <b>not</b> working';
+//const text = 'This is a [i]test[/i], all html tags should <b>not</b> working';
+//const text = 'This is a [b]test[/b], all html tags should <b>not</b> working';
 
 document.getElementById("btnSubmit").addEventListener("click", function(){
 
-	if(!text.includes('[b]') || !text.includes('[/b]')) {
-		addText(text);
+	if(isB(text) && !isI(text)) {
+		onlyB(text);
+	} else if(!isB(text) && isI(text)) {
+		onlyI(text);
+	} else if(isB(text) && isI(text)) {
+		bAndI(text);
 	} else {
-		let allText = text;
-		let beg, end = 0;
-		let part = ''; 
-
-
-		beg = allText.indexOf('[b]');
-		part = allText.slice(0, beg);
-		addText(part);
-		allText = allText.slice(beg, allText.length);
-
-
-		while(allText.includes('[b]') && allText.includes('[/b]')) {
-
-			beg = 3;
-			end = allText.indexOf('[/b]');
-			part = allText.slice(beg, end);
-			addElement(part);
-			allText = allText.slice(end+4, allText.length);
-
-			if(allText.includes('[b]') && allText.includes('[/b]')) {
-				beg = allText.indexOf('[b]');
-				part = allText.slice(0, beg);
-				addText(part);
-				allText = allText.slice(beg, allText.length);
-			}
-		}
-
-		part = allText.slice(0, allText.length);
-		addText(part);
-
-		test(allText);
+		addText(text);
 	}
 });
 
@@ -44,8 +20,8 @@ function addText(text) {
 	document.getElementById("main").appendChild(part);
 }
 
-function addElement(text) {
-    var element = document.createElement("B");
+function addElement(text, type) {
+    var element = document.createElement(type);
     var part = document.createTextNode(text);
     element.appendChild(part);
     document.getElementById("main").appendChild(element);
@@ -53,4 +29,96 @@ function addElement(text) {
 
 function test(text) {
 	console.log(text);
+}
+
+function isB(text) {
+	return text.includes('[b]') && text.includes('[/b]');
+}
+
+function isI(text) {
+	return text.includes('[i]') && text.includes('[/i]');
+}
+
+function bAndI(text) {
+	let allText = text;
+	let begB, endB, begI, endI = 0;
+	let part = ''; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	test(text);
+}
+
+function onlyB(text) {
+	let allText = text;
+	let beg, end = 0;
+	let part = ''; 
+
+	beg = allText.indexOf('[b]');
+	part = allText.slice(0, beg);
+	addText(part);
+	allText = allText.slice(beg, allText.length);
+
+	while(allText.includes('[b]') && allText.includes('[/b]')) {
+
+		beg = 3;
+		end = allText.indexOf('[/b]');
+		part = allText.slice(beg, end);
+		addElement(part, 'B');
+		allText = allText.slice(end+4, allText.length);
+
+		if(allText.includes('[b]') && allText.includes('[/b]')) {
+			beg = allText.indexOf('[b]');
+			part = allText.slice(0, beg);
+			addText(part);
+			allText = allText.slice(beg, allText.length);
+		}
+	}
+
+	part = allText.slice(0, allText.length);
+	addText(part);
+}
+
+function onlyI(text) {
+	let allText = text;
+	let beg, end = 0;
+	let part = ''; 
+
+	beg = allText.indexOf('[i]');
+	part = allText.slice(0, beg);
+	addText(part);
+	allText = allText.slice(beg, allText.length);
+
+	while(allText.includes('[i]') && allText.includes('[/i]')) {
+
+		beg = 3;
+		end = allText.indexOf('[/i]');
+		part = allText.slice(beg, end);
+		addElement(part, 'I');
+		allText = allText.slice(end+4, allText.length);
+
+		if(allText.includes('[i]') && allText.includes('[/i]')) {
+			beg = allText.indexOf('[i]');
+			part = allText.slice(0, beg);
+			addText(part);
+			allText = allText.slice(beg, allText.length);
+		}
+	}
+
+	part = allText.slice(0, allText.length);
+	addText(part);
 }
