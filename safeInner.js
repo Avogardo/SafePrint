36f03dce5f5c94ed04197148559fffb05535be85
Avogardo <avogardo0@gmail.com>
2017-02-01 17:06:32 +0100

    Safe print
    Zmieniono sposób podmieniania tekstu na znacznie prostszy

--- safeInner.js
+++ safeInner.js
@@ -1,198 +1,38 @@
-const text = '[i][/i][b][/b]<br><br>This is a test, all html tags<br>should not workin<br>g';
-//const text = 'This is a [i]test[/i], all html tags should <b>not</b> working';
-//const text = 'This is a [b]test[/b], all html tags should <b>not</b> working';
+const input = 'This is a test, here Im [i]testing a [b]ne[/i]w[/b] method of safe innering. For egample <p>paragraph tag shouldnt working</p>. <img src=x onerror=alert(1)>';
 
-document.getElementById("btnSubmit").addEventListener("click", function(){
+document.getElementById('btnSubmit').addEventListener('click', function() {
 
-	if(isB(text) && !isI(text)) {
-		onlyB(text);
-	} else if(!isB(text) && isI(text)) {
-		onlyI(text);
-	} else if(isB(text) && isI(text)) {
-		bAndI(text);
-	} else {
-		addText(text);
-	}
+	let text = input;
+	text = text.replace(/</g, '&lt;');
+	text = text.replace(/>/g, '&gt;');
+
+    text = text.replace(/\[b]/g, '<b>');
+    text = text.replace(/\[\/b]/g, '</b>');
+
+    text = text.replace(/\[i]/g, '<i>');
+    text = text.replace(/\[\/i]/g, '</i>');
+
+	console.log(text);
+
+	document.getElementById('main').innerHTML = text;
 });
 
-function addText(text) {
-	const part = document.createTextNode(text);
-	document.getElementById("main").appendChild(part);
-}
+document.getElementById('form').addEventListener('change', function() {
 
-function addElement(text, type) {
-	var element = document.createElement(type);
-	var part = document.createTextNode(text);
-	element.appendChild(part);
-	document.getElementById("main").appendChild(element);
-}
+    let text = document.getElementById('inbox').value;
 
-function addElementBr() {
-	var element = document.createElement('br');
-	document.getElementById("main").appendChild(element);
-}
+    text = text.replace(/</g, '&lt;');
+    text = text.replace(/>/g, '&gt;');
 
-function test(text) {
-	console.log(text);
-}
+    text = text.replace(/\[b]/g, '<b>');
+    text = text.replace(/\[\/b]/g, '</b>');
 
-function isB(text) {
-	return text.includes('[b]') && text.includes('[/b]');
-}
+    text = text.replace(/\[i]/g, '<i>');
+    text = text.replace(/\[\/i]/g, '</i>');
 
-function isI(text) {
-	return text.includes('[i]') && text.includes('[/i]');
-}
+    text = text.replace(/\r?\n/g, '<br>');
 
-function bAndI(text) {
-	let allText = text;
-	let beg, end = 0;
-	let part = ''; 
+    console.log(text);
 
-	beg = allText.indexOf('<br>');
-	part = allText.slice(0, beg);
-	addText(part);
-	allText = allText.slice(beg, allText.length);
-
-	while(allText.includes('<br>')) {
-
-		beg = 4;
-		end = allText.indexOf('<br>');
-		part = allText.slice(beg, end);
-		addElementBr();
-		allText = allText.slice(end+4, allText.length);
-
-		if(allText.includes('<br>')) {
-			beg = allText.indexOf('<br>');
-			part = allText.slice(0, beg);
-			addText(part);
-			allText = allText.slice(beg, allText.length);
-		}
-	}
-
-	part = allText.slice(0, allText.length);
-	addText(part);
-
-	test(text);
-}
-
-function onlyB(text) {
-	let allText = text;
-	let beg, end, begBR, endBR = 0;
-	let part = ''; 
-
-
-	while(allText.includes('[b]') && allText.includes('[/b]') || allText.includes('<br>')) {
-		if(!allText.includes('<br>')) {
-			beg = allText.indexOf('[b]');
-			part = allText.slice(0, beg);
-			addText(part);
-			allText = allText.slice(beg, allText.length);
-console.log('1');
-			while(allText.includes('[b]') && allText.includes('[/b]')) {
-
-				beg = 3;
-				end = allText.indexOf('[/b]');
-				part = allText.slice(beg, end);
-				addElement(part, 'B');
-				allText = allText.slice(end+4, allText.length);
-console.log('2');
-				if(allText.includes('[b]') && allText.includes('[/b]')) {
-					beg = allText.indexOf('[b]');
-					part = allText.slice(0, beg);
-					addText(part);
-					allText = allText.slice(beg, allText.length);
-console.log('3');
-				}
-			}
-
-			part = allText.slice(0, allText.length);
-			addText(part);
-		} else {
-			if(allText.includes('[b]') && allText.includes('[/b]')) {
-				beg = allText.indexOf('[b]');
-				begBR = allText.indexOf('<br>');
-
-				if(begBR < beg) {
-					part = allText.slice(0, begBR);
-					console.log(allText);
-					addText(part);
-					allText = allText.slice(begBR+4, allText.length);
-console.log('4');
-					endBR = 4;
-					part = allText.slice(0, endBR);
-					addElementBr();
-					allText = allText.slice(end, allText.length);
-console.log('5');
-				} else {
-					part = allText.slice(0, beg);
-					addText(part);
-					allText = allText.slice(beg, allText.length);
-console.log('6');
-					beg = 3;
-					end = allText.indexOf('[/b]');
-					part = allText.slice(beg, end);
-					addElement(part, 'B');
-					allText = allText.slice(end+4, allText.length);
-console.log('7');
-				}
-			} else {
-				while(allText.includes('<br>')) {
-console.log(allText);
-					begBR = allText.indexOf('<br>');
-					part = allText.slice(0, begBR);
-					addText(part);
-					allText = allText.slice(begBR, allText.length);
-console.log('8');
-
-console.log(allText);
-
-					if(allText.includes('<br>')) {
-console.log(allText);
-						endBR = 4;
-						allText = allText.slice(endBR, allText.length);
-console.log(allText);
-						addElementBr();
-						addText(allText);
-console.log(allText);
-console.log('9');
-					}
-				}
-			}
-		}
-	}
-
-
-	//addText(allText);
-console.log('10');
-}
-
-function onlyI(text) {
-	let allText = text;
-	let beg, end = 0;
-	let part = ''; 
-
-	beg = allText.indexOf('[i]');
-	part = allText.slice(0, beg);
-	addText(part);
-	allText = allText.slice(beg, allText.length);
-
-	while(allText.includes('[i]') && allText.includes('[/i]')) {
-
-		beg = 3;
-		end = allText.indexOf('[/i]');
-		part = allText.slice(beg, end);
-		addElement(part, 'I');
-		allText = allText.slice(end+4, allText.length);
-
-		if(allText.includes('[i]') && allText.includes('[/i]')) {
-			beg = allText.indexOf('[i]');
-			part = allText.slice(0, beg);
-			addText(part);
-			allText = allText.slice(beg, allText.length);
-		}
-	}
-
-	part = allText.slice(0, allText.length);
-	addText(part);
-}
+    document.getElementById('main').innerHTML = text;
+});
