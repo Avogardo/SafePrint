5a16f40eb784ef0b9851e2f6e72543c40f214858
Avogardo <avogardo0@gmail.com>
2017-02-01 17:11:25 +0100

    Safe inner

--- safeInner.js
+++ safeInner.js
@@ -2,9 +2,9 @@ const input = 'This is a test, here Im [i]testing a [b]ne[/i]w[/b] method of saf
 
 document.getElementById('btnSubmit').addEventListener('click', function() {
 
-	let text = input;
-	text = text.replace(/</g, '&lt;');
-	text = text.replace(/>/g, '&gt;');
+    let text = input;
+    text = text.replace(/</g, '&lt;');
+    text = text.replace(/>/g, '&gt;');
 
     text = text.replace(/\[b]/g, '<b>');
     text = text.replace(/\[\/b]/g, '</b>');
@@ -12,9 +12,9 @@ document.getElementById('btnSubmit').addEventListener('click', function() {
     text = text.replace(/\[i]/g, '<i>');
     text = text.replace(/\[\/i]/g, '</i>');
 
-	console.log(text);
+    console.log(text);
 
-	document.getElementById('main').innerHTML = text;
+    document.getElementById('main').innerHTML = text;
 });
 
 document.getElementById('form').addEventListener('change', function() {
